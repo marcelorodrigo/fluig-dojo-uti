d39f785ca9e798caecba0efb4bb8a8b7b9da6651
erickeuller <erickeuller@gmail.com>
2018-05-16 11:23:37 -0300

    100% mutation coverage

--- src/test/java/service/HospitalTest.java
+++ src/test/java/service/HospitalTest.java
@@ -2,7 +2,6 @@ package service;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import vo.Medicine;
 import vo.Patient;
 import vo.Prescription;
@@ -74,5 +73,47 @@ public class HospitalTest {
         Assert.assertTrue(triad.isAlarm());
     }
 
+    @Test
+    public void triad150() {
+        final Patient patient = new Patient()
+                .setWeight(85)
+                .setHeartBeat(150);
+        final Prescription triad = new Hospital().triad(patient);
+        Assert.assertEquals(Medicine.CALMANT, triad.getMedicine());
+        Assert.assertEquals(1.0 * 85, triad.getDose(), 0);
+        Assert.assertFalse(triad.isAlarm());
+    }
 
+    @Test
+    public void triad120() {
+        final Patient patient = new Patient()
+                .setWeight(85)
+                .setHeartBeat(120);
+        final Prescription triad = new Hospital().triad(patient);
+        Assert.assertNull(triad.getMedicine());
+        Assert.assertEquals(0, triad.getDose(), 0);
+        Assert.assertFalse(triad.isAlarm());
+    }
+
+    @Test
+    public void triad40() {
+        final Patient patient = new Patient()
+                .setWeight(85)
+                .setHeartBeat(40);
+        final Prescription triad = new Hospital().triad(patient);
+        Assert.assertNull(triad.getMedicine());
+        Assert.assertEquals(0, triad.getDose(), 0);
+        Assert.assertFalse(triad.isAlarm());
+    }
+
+    @Test
+    public void triad20() {
+        final Patient patient = new Patient()
+                .setWeight(85)
+                .setHeartBeat(20);
+        final Prescription triad = new Hospital().triad(patient);
+        Assert.assertEquals(Medicine.ADRENALINE, triad.getMedicine());
+        Assert.assertEquals(0.5 * 85, triad.getDose(), 0);
+        Assert.assertFalse(triad.isAlarm());
+    }
 }
